c8e7458f275bc806737c3f74c0f148d11f15a9d9
takewiki <hulilei@takewiki.com.cn>
2020-01-10 14:52:53 +0800

    initial setup

--- 01_row_body.R
+++ 01_row_body.R
@@ -1,47 +1,36 @@
 menu_row <- tabItem(tabName = "row",
                     fluidRow(
-                      column(width = 4,
+                      column(width = 3,
                              box(
-                               title = "row", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "外部标签", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_file('file_ext_barcode','选择外部标签文件'),
+                               mdl_ListChoose1('rule_ext_sorted','排序规则',list('二维码从小到大(升序)','二维码从大到小(降序)'),list(FALSE,TRUE),selected = FALSE),
+                               actionButton('btn_ext_barcode','预览外部标签')
                              ),
                              box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "LC内部标签", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_file('file_inner_barcode','选择内部标签文件')
                              ),
                              box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "匹配规则", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_ListChoose1('rule_ext_match','匹配规则',list('二维码从小到大(升序)','二维码从大到小(降序)'),list(FALSE,TRUE),selected = FALSE),
+                               actionButton('match_do','智能匹配'),
+                               mdl_download_button('match_dl','下载配货单')
                              )
                       ),
                       
-                      column(width = 4,
+                      column(width = 9,
                              box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "预览外部标签内容", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_dataTable('preview_ext_barcode',label = '预览外部标签内容')
                              ),
                              box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "预览内部标签内容", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_dataTable('preview_inner_barcode',label = '预览内部标签内容')
                              ),
                              box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
-                             )
-                      ),
-                      
-                      column(width=4,
-                             box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
-                             ),
-                             box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
-                             ),
-                             box(
-                               title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "预览配货单内容", width = NULL, solidHeader = TRUE, status = "primary",
+                               mdl_dataTable('preview_inner_barcode',label = '预览配货单内容')
                              )
                       )
                     )
--- server.R
+++ server.R
@@ -3,5 +3,31 @@
 #shinyserver start point----
  shinyServer(function(input, output,session) {
    
+  #1.预览外部标签内容------- 
+   file_ext_barcode <- var_file('file_ext_barcode')
+   rule_ext_sorted <- var_ListChoose1('rule_ext_sorted');
+   
+   data_ext_barcode <- reactive({
+     file_ext_barcode <- file_ext_barcode();
+     res <- readxl::read_excel(file_ext_barcode);
+     res <- res[,c('订单号',	'物料号'	,'二维码','印版线束')];
+     res <- res[order(res$`二维码`,decreasing = rule_ext_sorted()),]
+     return(res);
+     
+   })
+   
+   data_ext_barcode_pre <- reactive({
+     res <-data_ext_barcode()
+     res <- head(res)
+     return(res)
+     
+   })
+   
+   observeEvent(input$btn_ext_barcode,{
+     run_dataTable2('preview_ext_barcode',data_ext_barcode_pre())
+   })
+   
+  
+   
   
 })
